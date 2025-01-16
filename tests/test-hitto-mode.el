1c721b20af65f8fc3f2a60d9120b9958ebc5836f
Jared Arcilla <arcillajared@gmail.com>
2025-01-15 21:39:44 -0500

    More testing wip

--- tests/test-hitto-mode.el
+++ tests/test-hitto-mode.el
@@ -7,4 +7,16 @@
     (spy-on 'plz))
 
   (describe "caching"
-    (it "works" t)))
+    (it "works" t))
+
+  (describe "browsing"
+    (describe "hitto--read-start"
+      :var ((hitto--chapter-id "chapter-id")
+            (hitto--chapter-number "5")
+            (hitto--manga-name "My Manga")
+            (hitto--manga-id "manga-id"))
+
+      (it "creates a manga reading buffer"
+        (spy-on 'get-buffer-create)
+        (hitto--read-start "My Manga" 5)
+        (expect 'get-buffer-create :to-have-been-called-with "*My Manga*")))))
